87fb8151a298f3212f8d502f0bb11e04671126db
apepsiii <99173451+apepsiii@users.noreply.github.com>
2025-08-11 21:32:42 +0700

    commit ke dua

--- midtrans_config.py
+++ midtrans_config.py
@@ -2,6 +2,6 @@ import midtransclient
 
 core_api = midtransclient.CoreApi(
     is_production=True,  # Ubah ke True saat deploy live
-    server_key='Mid-server-MN0hH_SohpRMoX3HhPgwSPf9',
-    client_key='Mid-client-3QbvVj9HsOm0oIiL'
+    server_key='XXXX',
+    client_key='xxxx'
 )

fe2d1bc860cdbd64bb696d451c657c9e2dbadabc
apepsiii <99173451+apepsiii@users.noreply.github.com>
2025-08-13 23:46:02 +0700

    compress image png

--- midtrans_config.py
+++ midtrans_config.py
@@ -2,6 +2,6 @@ import midtransclient
 
 core_api = midtransclient.CoreApi(
     is_production=True,  # Ubah ke True saat deploy live
-    server_key='Mid-server-xxxxx',
+    server_key='Mid-server-xxxxxx',
     client_key='Mid-client-xxxxx'
 )
